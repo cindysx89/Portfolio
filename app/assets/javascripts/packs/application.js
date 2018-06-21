f96b68d67877508b727ddfd2d9b5cadbe856c008
Cindy Xie <cindyxie89@gmail.com>
2018-06-21 22:50:52 +0100

    added neptune and spaceship

---
+++ app/assets/javascripts/packs/application.js
@@ -0,0 +1,16 @@
+console.log("yay")
+
+ // document.querySelector("#neptune").onclick = function() {
+ //     document.querySelector("#neptune").style.visibility = "visible";
+ // }
+
+// const hidePlanet = document.querySelector("#neptune");
+
+// hidePlanet.addEventListener("scroll", event);
+
+// const event = (e) => {
+//   hidePlanet.style.visibility = "visible";
+// }
+
+
+document.querySelector("#neptune").classList.add("display");
